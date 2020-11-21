8272aa22f4b1e5d7d2712d5cabf36559ec10cc0c
TolpaBomjey <55511994+TolpaBomjey@users.noreply.github.com>
2020-11-21 21:56:51 +0300

    Delete task4.py

--- task4.py
+++
@@ -1,8 +0,0 @@
-userNum = input("теперь только целое положительное число >>>")
-start = 0
-maxNum = 0
-while (start < len(userNum)):
-    if (int(userNum[start]) > maxNum): maxNum = int(userNum[start])
-    start += 1
-
-print(maxNum)
